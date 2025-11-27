f1acf585d99ec3cd96b94d687434921734bf31fb
yeeun <jovm268@naver.com>
2025-11-27 13:51:30 +0900

    feat: select 공통 컴포넌트 props 제작 - props로 title과 placeholder 그리고 순회돌 데이터값도 임의로 정의하였음 - 어떻게 데이터 내려오는지, api 확인 필요 - onValueChange는 클릭시 선택되는 value값을 콘솔창에서 확인 할 수 있도록 설정함

--- src/pages/YeeunTest.tsx
+++ src/pages/YeeunTest.tsx
@@ -1,3 +1,20 @@
+import Select from '@/components/common/Select'
+
 export default function YeeunTest() {
-  return <div>YeeunTest</div>
+  const sampledata = [
+    { itemValue: '1', itemText: '사과' }, // itemValue에 서버 ID로 매핑
+    { itemValue: '2', itemText: '바나나' },
+    { itemValue: '123', itemText: '오렌지' },
+  ]
+  return (
+    <div>
+      YeeunTest
+      <Select
+        data={sampledata}
+        title="과일 선택"
+        placeHolder="좋아하는 과일을 고르세요"
+        onValueChange={(e) => console.log(e)} //디버깅
+      ></Select>
+    </div>
+  )
 }
