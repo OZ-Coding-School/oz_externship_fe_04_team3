b46e9602dc1dd1397e9f9232ba6795c4226ee5fa
agrade1 <kigf59@gmail.com>
2025-11-30 18:40:16 +0900

    feat: pages 배럴 파일 적용 - pages 컴포넌트 import 관리를 위한 배럴 파일 적용

---
+++ src/pages/postings/Manage.tsx
@@ -0,0 +1,3 @@
+export default function Manage() {
+  return <div>내가 작성한 공고 목록</div>
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,5 @@
 import Layout from '@/components/common/layout/Layout'
-import Courses from '@/pages/Courses'
-import Main from '@/pages/main'
-import YeeunTest from '@/pages/YeeunTest'
+import { Courses, Main, Manage, YeeunTest } from '@/pages'
 import { Route, Routes } from 'react-router'
 function AppRoutes() {
   return (
@@ -11,6 +9,7 @@ function AppRoutes() {
         <Route index element={<Main />} />
         <Route path="/courses" element={<Courses></Courses>}></Route>
         <Route path="/yeeun" element={<YeeunTest></YeeunTest>}></Route>
+        <Route path="/posting/manage" element={<Manage />}></Route>
       </Route>
     </Routes>
   )
