aeabd400ed6f3d9789731ef21f6848dcd5254f14
agrade1 <kigf59@gmail.com>
2025-12-01 11:10:12 +0900

    refactor: toast conatianer 적용 - 코드 공유시 누락됬던 toastContainer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import AppRoutes from '@/routes/AppRoutes'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
-  return <AppRoutes />
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        icon={false}
+        closeButton={false}
+      />
+    </>
+  )
 }
 export default App
