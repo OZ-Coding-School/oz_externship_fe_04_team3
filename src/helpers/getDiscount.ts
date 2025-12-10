7c7b9322e527f25681d3919b396be9b7a5fa89e4
yeeun <jovm268@naver.com>
2025-12-10 19:16:41 +0900

    chore: 할인율 계산 로직 수정 - 정가 대비 비율을 계산해야함 - 로직 수정

--- src/helpers/getDiscount.ts
+++ src/helpers/getDiscount.ts
@@ -1,3 +1,5 @@
 export function getDiscount(discounted_price: number, original_price: number) {
-  return Math.floor((discounted_price / original_price) * 100)
+  return Math.floor(
+    ((original_price - discounted_price) / original_price) * 100
+  )
 }
