4d48f8330ff51b9892030c8bd6006405de472a55
yeeun <jovm268@naver.com>
2025-12-12 15:39:54 +0900

    refactor: 리뷰카드 커스텀 - Dialog의 닫기 버튼 직접 선택 (Readix권장하는 slot='dialog-close' 사용 - 이 친구는 모달창의 커텐츠 영역 전부를 감싸고있음 - 그래서 overflow-y-auto 래퍼박스를 하나 만들었음

--- src/components/lecture/LectureReviewCard.tsx
+++ src/components/lecture/LectureReviewCard.tsx
@@ -40,7 +40,7 @@ export default function LectureReviewCard({
   } = lecture
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="m-0 p-0" showCloseButton>
+      <DialogContent className="m-0 p-0 **:data-[slot='dialog-close']:bg-gray-300">
         <Card className="w-full">
           <CardHeader>
             <CardAction className="absolute z-10 justify-between px-3 py-3">
@@ -59,43 +59,45 @@ export default function LectureReviewCard({
               loading="lazy"
             />
           </CardHeader>
-          <CardContent>
-            <div className="Card-Content-badge flex gap-1">
-              <Badge variant={'success'}>{LectureLevel[difficulty]}</Badge>
-              {categories?.map((i) => (
-                <Badge key={i.id}>{i.name}</Badge>
-              ))}
-            </div>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>{instructor}</CardDescription>
-            {getRatingStarsIcon(average_rating)}
-            <div className="Card-Content-price flex items-center gap-2">
-              <h4>₩{discounted_price.toLocaleString('ko-KR')}</h4>
-              <h6 className="text-sm text-gray-400 line-through">
-                ₩{original_price.toLocaleString('ko-KR')}
-              </h6>
-            </div>
-          </CardContent>
-          <CardFooter className="flex w-full flex-col items-start gap-2">
-            <CardTitle className="mb-2 text-left">리뷰보기</CardTitle>
-            {reviews.length === 0 ? (
-              <Item className="w-full">
-                <ItemContent>
-                  <ItemTitle>{getRatingStarsIcon(0)}</ItemTitle>
-                  <ItemDescription>리뷰가 없습니다</ItemDescription>
-                </ItemContent>
-              </Item>
-            ) : (
-              reviews.map((i) => (
-                <Item key={i.id} className="w-full">
+          <div className="review-content-section overflow-y-auto">
+            <CardContent>
+              <div className="Card-Content-badge flex gap-1">
+                <Badge variant={'success'}>{LectureLevel[difficulty]}</Badge>
+                {categories?.map((i) => (
+                  <Badge key={i.id}>{i.name}</Badge>
+                ))}
+              </div>
+              <CardTitle>{title}</CardTitle>
+              <CardDescription>{instructor}</CardDescription>
+              {getRatingStarsIcon(average_rating)}
+              <div className="Card-Content-price flex items-center gap-2">
+                <h4>₩{discounted_price.toLocaleString('ko-KR')}</h4>
+                <h6 className="text-sm text-gray-400 line-through">
+                  ₩{original_price.toLocaleString('ko-KR')}
+                </h6>
+              </div>
+            </CardContent>
+            <CardFooter className="mt-2 flex w-full flex-col items-start gap-2">
+              <CardTitle className="mb-2 text-left">리뷰보기</CardTitle>
+              {reviews.length === 0 ? (
+                <Item className="w-full">
                   <ItemContent>
-                    <ItemTitle>{getRatingStarsIcon(i.rating)}</ItemTitle>
-                    <ItemDescription>{i.content}</ItemDescription>
+                    <ItemTitle>{getRatingStarsIcon(0)}</ItemTitle>
+                    <ItemDescription>리뷰가 없습니다</ItemDescription>
                   </ItemContent>
                 </Item>
-              ))
-            )}
-          </CardFooter>
+              ) : (
+                reviews.map((i) => (
+                  <Item key={i.id} className="w-full">
+                    <ItemContent>
+                      <ItemTitle>{getRatingStarsIcon(i.rating)}</ItemTitle>
+                      <ItemDescription>{i.content}</ItemDescription>
+                    </ItemContent>
+                  </Item>
+                ))
+              )}
+            </CardFooter>
+          </div>
         </Card>
       </DialogContent>
     </Dialog>
