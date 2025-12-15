7620d87dd761ee2457ac1b3407668c2071be4112
yeeun <jovm268@naver.com>
2025-12-15 14:12:12 +0900

    chore: init - merge

---
+++ src/components/GuestRecommendSection.tsx
@@ -0,0 +1,49 @@
+import { LogIn, UserPlus } from 'lucide-react'
+import { useNavigate } from 'react-router'
+import { Button } from './common/Button'
+import CardSkeleton from './common/CardSkeleton'
+type DataProps = {
+  title: string
+  description: string
+}
+export default function GuestRecommendSection({
+  title,
+  description,
+}: DataProps) {
+  const navigate = useNavigate()
+  return (
+    <div className="border-primary-200 max-w-[1216px] rounded-xl border bg-linear-to-r from-[#FFF7ED] to-[#FEFCE8] px-4 py-8 md:p-8">
+      <div className="text-center">
+        <h3 className="text-balance text-gray-900">
+          개인 맞춤 {title} 받아보세요
+        </h3>
+        <p className="md:text-md mt-3 text-sm text-gray-500">
+          {description}를 추천해드립니다.
+        </p>
+        <div className="mt-6 flex items-center justify-center gap-x-4">
+          <Button variant={'primary'} onClick={() => navigate('/login')}>
+            <LogIn />
+            로그인하기
+          </Button>
+          <Button
+            variant={'outline-primary'}
+            onClick={() => navigate('/signup')}
+          >
+            <UserPlus />
+            회원가입 하기
+          </Button>
+        </div>
+        <div className="Skeleton-Section">
+          <p className="mt-8 mb-4 text-sm text-pretty text-gray-500">
+            로그인 후 이런 맞춤 추천을 받을 수 있어요
+          </p>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/lecture/LectureList.tsx
+++ src/components/lecture/LectureList.tsx
@@ -1,4 +1,3 @@
-import type { LoginState } from '@/store/loginStateStore'
 import type { LecturePageResponse } from '@/types/lecture'
 import type { InfiniteData } from '@tanstack/react-query'
 import React from 'react'
@@ -6,19 +5,14 @@ import LectureCard from './LectureCard'
 
 type dataProps = {
   data: InfiniteData<LecturePageResponse> | undefined
-  loginState: LoginState
 }
-export default function LectureList({ data, loginState }: dataProps) {
+export default function LectureList({ data }: dataProps) {
   return (
     <div className="grid w-full gap-3 sm:grid-cols-2 lg:grid-cols-3">
       {data?.pages.map((page, pageIndex) => (
         <React.Fragment key={pageIndex}>
           {page.results.map((lecture) => (
-            <LectureCard
-              key={lecture.id}
-              {...lecture}
-              loginState={loginState}
-            />
+            <LectureCard key={lecture.id} {...lecture} />
           ))}
         </React.Fragment>
       ))}
