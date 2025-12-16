8f20094cd7b1c5730d850b39350780abaf557706
yeeun <jovm268@naver.com>
2025-12-16 19:14:17 +0900

    feat: 에러 응답 인터셉터 추가 - 401에러시 토큰 재발급 - 인터셉터에 헤더로 다시 토큰값 실어서 보내기

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import { API_BASE_URL } from '@/constant/api'
 import { useAuthStore } from '@/store/userStore'
 import axios from 'axios'
+import { getAccessTokenApi } from './userInformation'
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -18,6 +19,35 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config
 })
+
+// 응답처리 401에러시 리프레쉬토큰 재발급
+// 재발급 받고 다시 요청헤더에 넣기.
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  async function (error) {
+    const originalRequest = error.config //에러헤더
+    const status = error.respone?.status //에러응답코드
+
+    if (status === 401) {
+      try {
+        const access_token = await getAccessTokenApi()
+        useAuthStore.getState().setAccessToken(access_token)
+        originalRequest.headers.Authorization = `Bearer ${access_token}`
+        return axiosInstance(originalRequest) //헤더에 토큰 다시 넣어서 재요청
+      } catch (refreshError) {
+        useAuthStore.getState().clearAuth()
+        //갱신 실패시, 로그인 페이지 이동
+        if (!window.location.pathname.includes('/login')) {
+          window.location.href = '/login'
+        }
+        return Promise.reject(refreshError)
+      }
+    }
+  }
+)
 
 // 스터디 공고 목록 조회
 export const getRecruitments = async (params: {
