61171f78191f22797d5c9bcd08ae702fbc7f191b
yeeun <jovm268@naver.com>
2025-12-16 11:47:59 +0900

    feat: accessToken 및 유저정보 get api 생성 - getUserInformationApi 로 유저 정보 받아오기 - getAccessTokenApi 로 액세스토큰 값 받아오기

--- src/api/userInformation.ts
+++ src/api/userInformation.ts
@@ -1,8 +1,16 @@
+import { axiosInstance } from '@/api/axios'
 import { API_PATHS } from '@/constant/api'
 import type { UserInformation } from '@/types/userInformation'
-import { axiosInstance } from '@/api/axios'
 
 export const getUserInformationApi = async (): Promise<UserInformation[]> => {
   const { data } = await axiosInstance.get(API_PATHS.USER.GET)
   return data
+}
+
+//리프레쉬토큰값 보내서 액세스토큰값 받아오기
+export const getAccessToken = async (): Promise<string> => {
+  const { data } = await axiosInstance.post<{ accessToken: string }>(
+    '/api/refresh'
+  )
+  return data.accessToken
 }
