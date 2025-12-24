034ec18f1d96506d30aacdf0fa94d113dfea002e
yeeun <jovm268@naver.com>
2025-12-24 16:21:00 +0900

    refactor: 추천 강의 불러오는 쿼리 위치 변경 - LectureRecommendSection 섹션 컴포넌트 내부에서 실시간으로 쿼리값 요청하여 데이터 받아오는걸로 수정 - 기존에서 사용한 쿼리훅은 제거함.

--- src/components/lecture/LectureRecommendSection.tsx
+++ src/components/lecture/LectureRecommendSection.tsx
@@ -1,12 +1,23 @@
-import useUserRecommendLecture from '@/hooks/useUserRecommendLecture'
+import { getUserRecommendsLecturesApi } from '@/api/lecture'
 import { useAuthStore } from '@/store/userStore'
+import { useQuery } from '@tanstack/react-query'
 import { Badge } from '../common/badge'
 import LectureCard from './LectureCard'
+import LectureCardSkeleton from './LectureCardSkeleton'
 
 export default function LectureRecommendSection() {
-  const { data } = useUserRecommendLecture(3)
-  /* 유저 상태 */
-  const { user } = useAuthStore()
+  /* 추천 강의 불러오기 */
+  const { user, loginState } = useAuthStore()
+  const { isLoading, data } = useQuery({
+    queryKey: ['lecture-recommend'],
+    queryFn: async () => {
+      console.time('API 호출 시간')
+      const data = await getUserRecommendsLecturesApi()
+      console.timeEnd('API 호출 시간')
+      return data
+    },
+    enabled: loginState === 'USER',
+  })
   return (
     <div className="border-primary-200 rounded-xl border bg-linear-to-r from-[#FFF7ED] to-[#FEFCE8] px-4 py-8 md:p-8">
       <div>
@@ -16,12 +27,19 @@ export default function LectureRecommendSection() {
           </h3>
           <Badge variant={'danger'}>개인맞춤</Badge>
         </div>
-
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {data?.map((i) => (
-            <LectureCard key={i.id} {...i}></LectureCard>
-          ))}
-        </div>
+        {isLoading ? (
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {[1, 2, 3].map((i) => (
+              <LectureCardSkeleton key={i} />
+            ))}
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {data?.map((i) => (
+              <LectureCard key={i.id} {...i}></LectureCard>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
