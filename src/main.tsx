fd36176a75c802cf0efd9d4c3ef51993d0ee9cb9
yeeun <jovm268@naver.com>
2025-12-25 23:38:13 +0900

    refactor: 리액트 쿼리 위치 재변경 및 코드 정리 - React Query 설정 개선 (staleTime 설정, devtools 연동) - API 함수의 불필요한 타입 선언 제거 (자동 추론 활용)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import App from './App'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router'
+// 리액트쿼리 데브툴 연동
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, //300초
+    },
+  },
+})
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient
+  }
+}
+window.__TANSTACK_QUERY_CLIENT__ = queryClient
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
@@ -16,15 +29,13 @@ async function enableMocking() {
     onUnhandledRequest: 'bypass',
   })
 }
-const queryClient = new QueryClient()
-enableMocking().then(() => {
+
+{
   createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
-})
+}
