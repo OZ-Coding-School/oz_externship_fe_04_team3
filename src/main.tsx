01c39b0fd94ab76aaf02832f88312d9dc6bd8119
yeeun <jovm268@naver.com>
2025-12-25 23:43:19 +0900

    refactor: 모킹데이터 삭제 - 모킹 삭제 - 강의 리스트 데이터 타입 정의

--- src/main.tsx
+++ src/main.tsx
@@ -18,18 +18,6 @@ declare global {
 }
 window.__TANSTACK_QUERY_CLIENT__ = queryClient
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
-
-  const { worker } = await import('./mocks/browser')
-
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  })
-}
-
 {
   createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
