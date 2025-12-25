71a8bd9e9b0a572b33cfad7514516042d74225e6
고연우 <roselite930118@gmail.com>
2025-12-25 23:54:40 +0900

    fix: 비로그인 상태 보호된 라우트 접근 처리 개선
    - 로그인 필요 토스트 메시지 추가
    - 존재하지 않는 /login라우트로 인한 404에러 방지

--- src/components/common/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute.tsx
@@ -1,5 +1,7 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import { useAuthStore } from '@/store/userStore'
+import { showToast } from '@/components/common/toast/Toast'
+import { useEffect } from 'react'
 
 interface ProtectedRouteProps {
   children: React.ReactNode
@@ -9,8 +11,20 @@ function ProtectedRoute({ children }: ProtectedRouteProps) {
   const loginState = useAuthStore((state) => state.loginState)
   const location = useLocation()
 
+  useEffect(() => {
+    if (loginState === 'GUEST') {
+      showToast.error('로그인 필요', '로그인이 필요한 페이지입니다.')
+    }
+  }, [loginState])
+
   if (loginState === 'GUEST') {
-    return <Navigate to="/login" replace state={{ from: location.pathname }} />
+    return (
+      <Navigate
+        to="/recruitments"
+        replace
+        state={{ from: location.pathname }}
+      />
+    )
   }
 
   return children
